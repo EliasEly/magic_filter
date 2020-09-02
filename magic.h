03179242a8844401a6d9349ef25dee47dd6e7767
Elias <elias.el@outlook.fr>
2020-09-02 15:34:19 +0200

    update prototypes

--- magic.h
+++ magic.h
@@ -3,17 +3,19 @@
 * Useful for the calls in main.c
 */
 
-void magicfilter1d_naive_(unsigned int *n, unsigned int *ndat, double const *source, double *dest);
+void magicfilter1d_naive_(int *n, int *ndat, double const *source, double *dest);
 
 void magicfilter1d_naive_bis_(unsigned int *n, unsigned int *ndat, double const *source, double *dest);
 
 void magicfilter1d_t_naive_(unsigned int *n, unsigned int *ndat, double const *source, double *dest);
 
-void magicfilter1d_naive_o1_(const unsigned int* restrict n, const unsigned int* restrict ndat,  const double* restrict source, double* restrict dest);
+void magicfilter1d_naive_o1_(const  int* restrict n, const  int* restrict ndat,  const double* restrict source, double* restrict dest);
 
-void magicfilter1d_naive_o2_(const unsigned int* restrict n, const unsigned int* restrict ndat,  const double* restrict source, double* restrict dest);
+void magicfilter1d_naive_o2_(const  int* restrict n, const  int* restrict ndat,  const double* restrict source, double* restrict dest);
 
-void magicfilter1d_naive_o3_(const unsigned int* restrict n, const unsigned int* restrict ndat,  const double* restrict source, double* restrict dest);
+void magicfilter1d_naive_o3_(const  int* restrict n, const  int* restrict ndat,  const double* restrict source, double* restrict dest);
+
+void magicfilter1d_naive_o4_(const  int* restrict n, const  int* restrict ndat,  const double* restrict source, double* restrict dest);
 
 void magicfilter1d_sse_(unsigned int *n, unsigned int *ndat, double const *source, double *dest);
 
